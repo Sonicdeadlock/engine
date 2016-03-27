0944be2c358935b1cc0b9d93e11a9de373851fdc
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-03-27 18:45:26 -0400

    reformatted text engine

--- routes/api/textEngine.js
+++ routes/api/textEngine.js
@@ -5,46 +5,18 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../db');
 var _ = require('lodash');
+var textMod = require('../../classes/textMod');
 
-var char_replaceModel = require('../../models/char_replace');
-var char_replace = db.model('character_replacements');
 
 router.route('/remove').post(function(req,res){
-   var text = req.body.text;
-    var remove = req.body.remove;
-    res.send(text.split(remove).join(''));
+    res.send(textMod.remove(req.body.remove,req.body.text));
 });
 
 router.route('/leet').post(function(req,res){
-    var chance = req.body.chance/100;
-    char_replace.find({}).then(function(replacements){
-        var result = _.map(req.body.text.split(' '),function(word){
-            word = _.map(word,function(character){
-                if(Math.random()>chance)//if the chance is not meet dont chance the character
-                    return character;
-                var options = _.find(replacements,{character: _.toLower(character)});
-                if(options ==undefined )//if the character has no replacements dont' change it
-                    return character;
-                return _.sample(options.replacements);
-
-            }).join('');
-            if(Math.random()>chance/10)
-            return word+' ';
-            var word_options = [
-                function(word){
-                    return "<<"+word+">> ";
-                },
-                function(word){
-                    return "<"+word+"> ";
-                },
-                function(word){
-                    return ""+word+"_";
-                }
-            ];
-            return _.sample(word_options)(word);
-        }).join('');
-        res.send(result);
-    })
+    textMod.leet(req.body.text,req.body.chance)
+        .then(function(result){
+            res.send(result);
+        })
 });
 
 module.exports = router;
