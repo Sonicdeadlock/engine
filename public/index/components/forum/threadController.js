ae67c9c246e99fe52699b4ff11749fadcce7a6ab
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-05-06 20:00:26 -0400

    markdown broke the forum thread event log but that has been fixed

--- public/index/components/forum/threadController.js
+++ public/index/components/forum/threadController.js
@@ -13,10 +13,9 @@ angular.module('controllers').controller('threadController',function($scope,$htt
                     $scope.atBottomOfThread=false;
                     data.posts.forEach(function(post){
                         post.htmlBody = markdown.toHTML(post.body);
-                        console.log(markdown.toHTML(post.body))
                     });
                     data.history.forEach(function(event){
-                       data.posts.push({body:event.actor.username+" "+event.action+'ed this post at '+event.date,creationTime:event.date,lastUpdateTime:event.date,_id:'notanID'})
+                       data.posts.push({htmlBody:event.actor.username+" "+event.action+'ed this post at '+event.date,creationTime:event.date,lastUpdateTime:event.date,_id:'notanID'})
                     });
                     $scope.thread = data;
 
