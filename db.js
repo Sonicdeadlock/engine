023bf61629ba8680528e1c364f4f6e76011467ba
Alex Thomas <athomas369@gmail.com>
2016-12-26 12:18:29 -0500

    use native promises in mongoose

--- db.js
+++ db.js
@@ -4,7 +4,7 @@
 var config = require('./config');
 var mongoose = require('mongoose');
 require('mongoose-cache').install(mongoose, {max: 500, maxAge: 1000 * 60 * 2, debug: true});
-
+mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://' + config.db.host + '/' + config.db.name)
     .connection
     .on('error', function (err) {
