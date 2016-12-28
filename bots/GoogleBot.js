ada927b202aca977e098b70e30d2e8e2360f93ba
Sonicdeadlock <sonicdeadlock369@gmail.com>
2016-12-28 13:50:00 -0500

    created google bot

---
+++ bots/GoogleBot.js
@@ -0,0 +1,23 @@
+var chat = require('../chat');
+var Room = require('../models/room');
+var _ = require('lodash');
+var db = require('../db');
+
+function chatInduction(user, room, chat, roomChatCallback, userChatCallback) {
+  if(_.startsWith(chat,'!g'){
+      var searchQuery = _.tail(chat.split(' '));
+      var response = "<a href='https://www.google.com/#q="+searchQuery.join('+')+"' target='_blank'>Find your search here</a>";
+      var roomChatCallback(response);
+  }
+}
+
+function init() {
+    Room.find({bots: {"$elemMatch": {name: "basic"}}}).then(function (roomResults) {
+        roomResults.forEach(function (room) {
+            chat.on("chat", room._id, function (user, chatToRoom, chatToUser, text) {
+                chatInduction(user, room, text, chatToRoom, chatToUser);
+            });
+        });
+    });
+}
+module.exports.init = init;
