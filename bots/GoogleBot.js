93fcf3c81a0799ca35a31e3533e38e48f4d89322
Alex Thomas <athomas369@gmail.com>
2016-12-28 18:13:06 -0500

    bind the bot to all the rooms so it doesn't need to be added

--- bots/GoogleBot.js
+++ bots/GoogleBot.js
@@ -4,15 +4,15 @@ var _ = require('lodash');
 var db = require('../db');
 
 function chatInduction(user, room, chat, roomChatCallback, userChatCallback) {
-  if(_.startsWith(chat,'!g'){
+  if(_.startsWith(chat,'!g')){
       var searchQuery = _.tail(chat.split(' '));
       var response = "<a href='https://www.google.com/#q="+searchQuery.join('+')+"' target='_blank'>Find your search here</a>";
-      var roomChatCallback(response);
+       roomChatCallback(response);
   }
 }
 
 function init() {
-    Room.find({bots: {"$elemMatch": {name: "google"}}}).then(function (roomResults) {
+    Room.find({}).then(function (roomResults) {
         roomResults.forEach(function (room) {
             chat.on("chat", room._id, function (user, chatToRoom, chatToUser, text) {
                 chatInduction(user, room, text, chatToRoom, chatToUser);
