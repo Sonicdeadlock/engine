fab7ae3c99b63b6187b15076cd1770b28b789848
Alex Thomas <athomas369@gmail.com>
2016-12-29 05:12:43 -0500

    google bot sends a user to chat

--- bots/GoogleBot.js
+++ bots/GoogleBot.js
@@ -2,15 +2,22 @@ var chat = require('../chat');
 var Room = require('../models/room');
 var _ = require('lodash');
 var db = require('../db');
-
+const baseResponse = {text:'',time:_.now(),username:'GoogleBot',rank:'Bot'};
 function chatInduction(user, room, chat, roomChatCallback, userChatCallback) {
+    var responseText=undefined;
   if(_.startsWith(chat,'!g')){
       var searchQuery = _.tail(chat.split(' '));
-      var response = "<a href='https://www.google.com/#q="+searchQuery.join('+')+"' target='_blank'>Find your search here</a>";
+      responseText = "<a href='https://www.google.com/#q="+searchQuery.join('+')+"' target='_blank'>Find your search here</a>";
        roomChatCallback(response);
   }else if(_.startsWith(chat,'!lmgtfy')){
       var searchQuery = _.tail(chat.split(' '));
-      roomChatCallback('<a href="http://lmgtfy.com/?q='+searchQuery.join('+')+'" target="_blank">Find your search here</a>');
+      responseText = '<a href="http://lmgtfy.com/?q='+searchQuery.join('+')+'" target="_blank">Find your search here</a>';
+  }
+  if(responseText!==undefined){
+      var response = _.cloneDeep(baseResponse);
+      response.time = _.now();
+      response.text = responseText;
+      roomChatCallback(response);
   }
 }
 
